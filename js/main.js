934a402a749216e20bf919a18a8a9ec4bb3a7a85
Mehedi Hasan Nayeem <mehedinayeem@Mehedis-MacBook-Air.local>
2024-05-04 17:57:26 +0600

    Complete numeral to Roman converter functionality

--- js/main.js
+++ js/main.js
@@ -30,20 +30,92 @@ const converFunction = (input) => {
         default:
             break;
     }
-    
+}
+
+const printRomanNumber = (nearest, number) => {
+    let result = ''
+    for (let index = 0; index < number; index++) {
+        result += nearest   
+    }
+    return result
+    // console.log(result)
+}
+
+
+printRomanNumber("V", 1)
+
+
+const finalResult = (nearest, base, highest, inputNumber) => {
+    const result =  inputNumber - 5
+    let romanNumber 
+    if(result === 0){
+        romanNumber = base
+    }
+    else if(result === -1){
+        romanNumber = nearest + base
+    }
+    else if(result === 4){
+        romanNumber = nearest + highest
+    }
+    else if(result <= -2){
+        romanNumber = printRomanNumber(nearest, 5 - Math.abs(result))
+    }
+    else if(result <= 3){
+        romanNumber = base + printRomanNumber(nearest, result)
+    }
+    return romanNumber
+}
+
+
+
+const lastResult = (input) => {
+    const digitsArray = String(input).split('').map(Number);
+    const digitsArrayLength = digitsArray.length
+    let tenth, hundredth, thousandth, fifthThoundanth
+    if(digitsArrayLength >= 1){
+        tenth = finalResult("I", "V", "X", digitsArray[digitsArrayLength -1])
+    }
+    if(digitsArrayLength >= 2){
+        hundredth = finalResult("X", "L", "C", digitsArray[digitsArrayLength -2])
+    }
+    if(digitsArrayLength >= 3){
+        thousandth = finalResult("C", "D", "V̅", digitsArray[digitsArrayLength -3])
+    }
+    if(digitsArrayLength >= 4){
+        fifthThoundanth = finalResult("D", "V̅", "X̅ ", digitsArray[digitsArrayLength -4])
+    }
+    let veryLastResult
+    if(fifthThoundanth) {
+        veryLastResult = fifthThoundanth + thousandth + hundredth + tenth
+    }
+    else if(thousandth) {
+        veryLastResult = thousandth + hundredth + tenth
+    }
+    else if(hundredth) {
+        veryLastResult = hundredth + tenth
+    }
+    else if(tenth) {
+        veryLastResult =  tenth
+    }
+    return veryLastResult
 }
 
 btn.addEventListener('click', function() {
     const inputValue = document.querySelector('.input-number');
     const inputValueNumber = Number(inputValue.value)
 
-    console.log(inputValueNumber + 1)
-    if(inputValueNumber < 0){
+    if(inputValueNumber <= 0){
         outputDiv.classList.add('danger')
         outputText.textContent = "Please enter a number greater than or equal to 1"
     }
-    if(inputValueNumber >= 4000){
+    else if(inputValueNumber < 4000){
+        outputDiv.classList.add('primary')
+        outputDiv.classList.remove('danger')
+        outputText.textContent = lastResult(inputValueNumber)
+    }
+    else if(inputValueNumber >= 4000){
         outputDiv.classList.add('danger')
+        outputDiv.classList.remove('primary')
         outputText.textContent = "Please enter a number less than or equal to 3999"
     }
 });
